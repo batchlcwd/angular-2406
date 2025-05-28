c829d24c5521aecd5c3cabe5740bb39549675b8e
batchlcwd <batchlcwd@gmail.com>
2025-05-28 22:17:48 +0530

    28:5 api calling using HttpClient

---
+++ weather-app/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatButtonModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'weather-app';
+
+  user!: any;
+
+  constructor(private http: HttpClient) {}
+
+  getData() {
+    this.http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe({
+      next: (data) => {
+        console.log('Data received:', data);
+        this.user = data;
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+      },
+    });
+
+    console.log('Data fetched successfully');
+  }
+}
